16ce97daddc16bc5c84b9975b95523ebd24d5f10
George Torres <george.torres.641@gmail.com>
2024-11-20 11:08:27 -0800

    testing express stuff

--- app.js
+++ app.js
@@ -12,16 +12,31 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/login', (req, res) => {
+  res.render('login', {title: 'Login'});
+});
+
+app.get('/register', (req, res) => {
+  res.render('register', {title: 'Register'});
+});
+
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.path}`);
+    next();
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
