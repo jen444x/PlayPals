c995a6eca1c3580396ec7deab17100393e3bef25
vfigueroa721 <vfigueroa7@csub.edu>
2025-03-13 11:32:44 -0700

    themes for dark mode

---
+++ ThemeContext.js
@@ -0,0 +1,14 @@
+// ThemeContext.js
+import React, { createContext, useState } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
