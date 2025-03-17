482b795c0ad9f4f613d001144c88a843ba11322f
vfigueroa721 <vfigueroa7@csub.edu>
2025-03-17 10:54:39 -0700

    add new files

--- App.js
+++ App.js
@@ -7,6 +7,9 @@ import HomeScreen from './screens/HomeScreen';
 import PetProfile from './screens/PetProfile';
 import AddPet from './screens/AddPet';
 import EditPet from './screens/EditPet';
+import ForumsScreen from './screens/ForumsScreen';
+import NewDiscussion from './screens/NewDiscussion';
+import DiscussionDetail from './screens/DiscussionDetail';
 import CalendarScreen from './screens/CalendarScreen';
 import AppSettings from './screens/AppSettings';
 import UserProfile from './screens/UserProfile';
@@ -25,6 +28,9 @@ export default function App() {
           <Stack.Screen name="PetProfile" component={PetProfile} options={{ title: 'Pet Profile' }} />
           <Stack.Screen name="AddPet" component={AddPet} options={{ title: 'Add a Pet' }} />
           <Stack.Screen name="EditPet" component={EditPet} options={{ title: 'Edit Pet' }} />
+          <Stack.Screen name="Forums" component={ForumsScreen} options={{ title: 'Forums' }} />
+          <Stack.Screen name="NewDiscussion" component={NewDiscussion} options={{ title: 'New Discussion' }} />
+          <Stack.Screen name="DiscussionDetail" component={DiscussionDetail} options={{ title: 'Discussion Detail' }} />
           <Stack.Screen name="Calendar" component={CalendarScreen} options={{ title: 'Pet Calendar' }} />
           <Stack.Screen name="AppSettings" component={AppSettings} options={{ title: 'App Settings' }} />
           <Stack.Screen name="UserProfile" component={UserProfile} options={{ title: 'User Profile' }} />
